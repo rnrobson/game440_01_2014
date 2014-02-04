eeb01f86a8c54f384c83aca23a69bd903d860169
Andrew Godfroy <andrew.godfroy@killerrin.com>
2014-02-04 01:50:51 -0500

    Client Main.cpp
    Removing build error until Error can be solved

--- Implementation/Client/Client/main.cpp
+++ Implementation/Client/Client/main.cpp
@@ -1,3 +1,4 @@
+//#include "../ClientAPI/ClientAPI.h"
 #include "ClientAPI.h"
 //#include "Window.h"
 
@@ -7,33 +8,33 @@ void Draw();
 
 int main(int argc, char* args[])
 {
-	//-- Initialize the API
-	ClientAPI::Init();
+	////-- Initialize the API
+	//ClientAPI::Init();
 
-	ClientAPI::SetCustomUpdate(Update);
-	ClientAPI::SetCustomDraw(Draw);
+	//ClientAPI::SetCustomUpdate(Update);
+	//ClientAPI::SetCustomDraw(Draw);
 
-	//-- Add in some Sample Assets
-	ClientAPI::AddColour("Colour1", APIHelper::ColourHelper(155, 205, 225, 205));//SDL_Color colour = { 155, 205, 225, 205 };
-	ClientAPI::AddFont("OGWEAR", Window::LoadFont("Resources/Fonts/OGWEAR.ttf", 24));//TTF_Font* font = Window::LoadFont("OGWEAR.ttf", 24);
+	////-- Add in some Sample Assets
+	//ClientAPI::AddColour("Colour1", APIHelper::ColourHelper(155, 205, 225, 205));//SDL_Color colour = { 155, 205, 225, 205 };
+	//ClientAPI::AddFont("OGWEAR", Window::LoadFont("Resources/Fonts/OGWEAR.ttf", 24));//TTF_Font* font = Window::LoadFont("OGWEAR.ttf", 24);
 
-	ClientAPI::AddButton("Button1", new Button(APIHelper::RectHelper(200, 200, 200, 100)));//Button* b = new Button(Window::RectHelper(200, 200, 200, 100));
-	ClientAPI::GetButton("Button1")->AddLabel("Button Label 25 Button Label 255", ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1"), true);// font, colour, true);
+	//ClientAPI::AddButton("Button1", new Button(APIHelper::RectHelper(200, 200, 200, 100)));//Button* b = new Button(Window::RectHelper(200, 200, 200, 100));
+	//ClientAPI::GetButton("Button1")->AddLabel("Button Label 25 Button Label 255", ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1"), true);// font, colour, true);
 
-	ClientAPI::AddLabel("Label1", new Label("Text Label 1", APIHelper::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1")));// Label* l = new Label("Text Label 1", Window::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), colour);// font, colour);
+	//ClientAPI::AddLabel("Label1", new Label("Text Label 1", APIHelper::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1")));// Label* l = new Label("Text Label 1", Window::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), colour);// font, colour);
 
-	ClientAPI::AddGuiContainer("Container 1", new GuiContainer());
-	ClientAPI::GetGuiContainer("Container 1")->AddButton("C Button 1", new Button(APIHelper::RectHelper(300, 500, 100, 100)));
-	ClientAPI::GetGuiContainer("Container 1")->AddLabel("C Label1", new Label("C Text Label 1", APIHelper::RectHelper(200, 600, 200, 100), ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1")));// Label* l = new Label("Text Label 1", Window::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), colour);// font, colour);
-	ClientAPI::GetGuiContainer("Container 1")->SetOffset(APIHelper::RectHelper(200, -200, 0, 0));
+	//ClientAPI::AddGuiContainer("Container 1", new GuiContainer());
+	//ClientAPI::GetGuiContainer("Container 1")->AddButton("C Button 1", new Button(APIHelper::RectHelper(300, 500, 100, 100)));
+	//ClientAPI::GetGuiContainer("Container 1")->AddLabel("C Label1", new Label("C Text Label 1", APIHelper::RectHelper(200, 600, 200, 100), ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Colour1")));// Label* l = new Label("Text Label 1", Window::RectHelper(600, 200, 200, 100), ClientAPI::GetFont("OGWEAR"), colour);// font, colour);
+	//ClientAPI::GetGuiContainer("Container 1")->SetOffset(APIHelper::RectHelper(200, -200, 0, 0));
 
-	ClientAPI::RemoveLabel("Label1");
-	ClientAPI::GetGuiContainer("Container 1")->Active = false;
+	//ClientAPI::RemoveLabel("Label1");
+	//ClientAPI::GetGuiContainer("Container 1")->Active = false;
 
-	//-- Start the APIs main loop
-	ClientAPI::BeginMainLoop();
-	//-- Quit the API once the APIs Main loop is over
-	ClientAPI::Quit();
+	////-- Start the APIs main loop
+	//ClientAPI::BeginMainLoop();
+	////-- Quit the API once the APIs Main loop is over
+	//ClientAPI::Quit();
 	return 0;
 }
 
@@ -42,7 +43,7 @@ void Update(double time)
 	cout << "Entering Custom Update" << endl;
 
 	SDL_Delay(1000);
-	ClientAPI::ExitMainLoop();
+	//ClientAPI::ExitMainLoop();
 }
 
 void Draw()
