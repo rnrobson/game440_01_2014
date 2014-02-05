df49e1c48f782df759257b7f62e263840fc1e7d3
Christan Adao <adaoc01@gmail.com>
2014-02-05 17:26:25 -0500

    Revised Rough Implementation of the Game Lobby

--- Implementation/Client/Client/GameLobby.h
+++ Implementation/Client/Client/GameLobby.h
@@ -6,81 +6,104 @@ class GameLobby
 public:
 	static void Load()
 	{
-		ClientAPI::AddFont("OGWEAR", Window::LoadFont("Resources/Fonts/OGWEAR.ttf", 24));//TTF_Font* font = Window::LoadFont("OGWEAR.ttf", 24);
+		const int SCREEN_WIDTH = Window::Box().w;
+		const int SCREEN_HEIGHT = Window::Box().h;
 
+		//add font
+		ClientAPI::AddFont("OGWEAR", Window::LoadFont("Resources/Fonts/OGWEAR.ttf", 36));//TTF_Font* font = Window::LoadFont("OGWEAR.ttf", 24);
+		ClientAPI::AddFont("Systema_11", Window::LoadFont("Resources/Fonts/9SYSTEMA.ttf", 11));
+		ClientAPI::AddFont("Systema_22", Window::LoadFont("Resources/Fonts/9SYSTEMA.ttf", 22));
 		//add colors to be used for buttons, labels, etc
 		ClientAPI::AddColour("White", APIHelper::ColourHelper(255, 255, 255, 255));
-		ClientAPI::AddColour("Grey", APIHelper::ColourHelper(240, 240, 240, 50));
 		ClientAPI::AddColour("LightBlue", APIHelper::ColourHelper(0, 162, 232, 255));
-		ClientAPI::AddColour("DarkBlue", APIHelper::ColourHelper(0, 110, 155, 50));
 		ClientAPI::AddColour("Red", APIHelper::ColourHelper(255, 0, 0, 255));
-		ClientAPI::AddColour("DarkRed", APIHelper::ColourHelper(165, 0, 0, 50));
+		
+		//load button textures for different states
+		SDL_Texture *smallBtnNormalTex = APIHelper::LoadBMPImage("Resources/GUITextures/smallBtnNormal.bmp");
+		SDL_Texture *smallBtnHoverTex = APIHelper::LoadBMPImage("Resources/GUITextures/smallBtnHover.bmp");
+		SDL_Texture *smallBtnDownTex = APIHelper::LoadBMPImage("Resources/GUITextures/smallBtnDown.bmp");
+		SDL_Texture *medBtnNormalTex = APIHelper::LoadBMPImage("Resources/GUITextures/medBtnNormal.bmp");
+		SDL_Texture *medBtnHoverTex = APIHelper::LoadBMPImage("Resources/GUITextures/medBtnHover.bmp");
+		SDL_Texture *medBtnDownTex = APIHelper::LoadBMPImage("Resources/GUITextures/medBtnDown.bmp");
 
-		//background
-		//ClientAPI::AddTexture("bg", APIHelper::LoadPNGTexture("bg.png"));
+		//add the textures
+		ClientAPI::AddTexture("SmallBtnNormal", smallBtnNormalTex);
+		ClientAPI::AddTexture("SmallBtnHover", smallBtnHoverTex);
+		ClientAPI::AddTexture("SmallBtnDown", smallBtnDownTex);
+		ClientAPI::AddTexture("MedBtnNormal", medBtnNormalTex);
+		ClientAPI::AddTexture("MedBtnHover", medBtnHoverTex);
+		ClientAPI::AddTexture("MedBtnDown", medBtnDownTex);
 
-		//add labels to containers
-		//"Illuminated" text and its shadow
-		ClientAPI::AddGuiContainer("iCon", new GuiContainer());
-		ClientAPI::GetGuiContainer("iCon")->AddLabel(
-			"Illuminated_b", new Label("The Illuminated", APIHelper::RectHelper(71, 51, 200, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("DarkBlue"))
-			);
-		ClientAPI::GetGuiContainer("iCon")->AddLabel(
-			"Illuminated", new Label("The Illuminated", APIHelper::RectHelper(70, 50, 200, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("LightBlue"))
-			);
+		//"Illuminated" text
+		ClientAPI::AddLabel("Illuminated", 
+			new Label("The Illuminated", APIHelper::RectHelper(70, 50, 300, 100),
+			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("LightBlue")));
 
-		//"Dark Ones" text and its shadow
-		ClientAPI::AddGuiContainer("doCon", new GuiContainer());
-		ClientAPI::GetGuiContainer("doCon")->AddLabel(
-			"DarkOnes_b", new Label("The Dark Ones", APIHelper::RectHelper(775, 51, 200, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("DarkRed"))
-			);
-		ClientAPI::GetGuiContainer("doCon")->AddLabel(
-			"DarkOnes", new Label("The Dark Ones", APIHelper::RectHelper(774, 50, 200, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Red"))
-			);
+		//"Dark Ones" text
+		ClientAPI::AddLabel("DarkOnes", 
+			new Label("The Dark Ones", APIHelper::RectHelper(SCREEN_WIDTH - 340, 50, 300, 100),
+			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Red")));
 
-		//"VS" text and its shadow
-		ClientAPI::AddGuiContainer("vsCon", new GuiContainer());
-		ClientAPI::GetGuiContainer("vsCon")->AddLabel(
-			"VS_b", new Label("- VS -", APIHelper::RectHelper(473, 180, 100, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("Grey"))
-			);
-		ClientAPI::GetGuiContainer("vsCon")->AddLabel(
-			"VS", new Label("- VS -", APIHelper::RectHelper(472, 181, 100, 100),
-			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("White"))
-			);
+		//"vs" text
+		ClientAPI::AddLabel("Versus",
+			new Label("Versus", APIHelper::RectHelper((SCREEN_WIDTH / 2) - 60, 70, 200, 100),
+			ClientAPI::GetFont("OGWEAR"), ClientAPI::GetColor("White")));
 
 		//add join and leave buttons to container
 		ClientAPI::AddGuiContainer("teamAllocBtnsCon", new GuiContainer());
-		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->AddButton(
-			"JoinI", new Button(APIHelper::RectHelper(100, 450, 100, 50))
-			);
-		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->AddButton(
-			"JoinDO", new Button(APIHelper::RectHelper(814, 450, 100, 50))
-			);
-		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->AddButton(
-			"backToBench", new Button(APIHelper::RectHelper(462, 490, 100, 50))
-			);
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->
+			AddButton("JoinI", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(100, 450, 100, 50)));
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->
+			AddButton("JoinDO", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(SCREEN_WIDTH - 200, 450, 100, 50)));
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->
+			AddButton("backToBench", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper((SCREEN_WIDTH / 2) - 50, 490, 100, 60)));
 
 		//add hosting and game options buttons to container
 		ClientAPI::AddGuiContainer("gameSetBtnsCon", new GuiContainer());
-		ClientAPI::GetGuiContainer("gameSetBtnsCon")->AddButton(
-			"startGame", new Button(APIHelper::RectHelper(900, 625, 100, 20))
-			);
-		ClientAPI::GetGuiContainer("gameSetBtnsCon")->AddButton(
-			"closeGame", new Button(APIHelper::RectHelper(900, 650, 100, 20))
-			);
-		ClientAPI::GetGuiContainer("gameSetBtnsCon")->AddButton(
-			"gameOptions", new Button(APIHelper::RectHelper(900, 675, 100, 20))
-			);
-		ClientAPI::GetGuiContainer("gameSetBtnsCon")->AddButton(
-			"returnToMainMenu", new Button(APIHelper::RectHelper(900, 725, 100, 20))
-			);
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->
+			AddButton("startGame", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(900, 600, 100, 30)));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->
+			AddButton("closeGame", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(900, 635, 100, 30)));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->
+			AddButton("gameOptions", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(900, 675, 100, 30)));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->
+			AddButton("returnToMainMenu", new Button(ClientAPI::GetTexture("MedBtnNormal"), APIHelper::RectHelper(900, 725, 100, 30)));
 
-		//adjust the containers
+		//add texts to buttons
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("JoinI")->
+			AddLabel("JOIN", ClientAPI::GetFont("Systema_22"), ClientAPI::GetColor("LightBlue"), false);
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("JoinDO")->
+			AddLabel("JOIN", ClientAPI::GetFont("Systema_22"), ClientAPI::GetColor("Red"), false);
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("backToBench")->
+			AddLabel("Leave Team", ClientAPI::GetFont("Systema_22"), ClientAPI::GetColor("Black"), false);
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("startGame")->
+			AddLabel("Start Game", ClientAPI::GetFont("Systema_11"), ClientAPI::GetColor("Black"), false);
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("closeGame")->
+			AddLabel("Close Game", ClientAPI::GetFont("Systema_11"), ClientAPI::GetColor("Black"), false);
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("gameOptions")->
+			AddLabel("Game Options", ClientAPI::GetFont("Systema_11"), ClientAPI::GetColor("Black"), false);
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("returnToMainMenu")->
+			AddLabel("Back", ClientAPI::GetFont("Systema_11"), ClientAPI::GetColor("Black"), false);
+		
+		//position the texts on buttons
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("JoinI")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(18, 12, 0, 0));
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("JoinDO")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(18, 12, 0, 0));
+		ClientAPI::GetGuiContainer("teamAllocBtnsCon")->GetButton("backToBench")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(12, 3, 0, 0));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("startGame")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(12, 8, 0, 0));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("closeGame")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(11, 8, 0, 0));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("gameOptions")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(7, 8, 0, 0));
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("returnToMainMenu")->
+			GetLabel()->SetPadding(APIHelper::RectHelper(29, 8, 0, 0));
+
+		//add events to buttons
+		ClientAPI::GetGuiContainer("gameSetBtnsCon")->GetButton("returnToMainMenu")->
+			SubscribeOnMouseClick(Quit);
 	}
 	static void Update(double _time) //might not be needed
 	{
@@ -91,7 +114,7 @@ public:
 	}
 	static void Quit()
 	{
-
+		ClientAPI::ExitMainLoop();
 	}
 };
 
