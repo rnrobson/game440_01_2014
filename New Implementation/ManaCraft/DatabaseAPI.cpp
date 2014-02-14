4057e3dcfea4803ca7e16c516b201325a14a23e1
Vinny Crupi <vinnycrupi@hotmail.com>
2014-02-13 23:19:37 -0500

    Some DatabaseAPI implementation
    DatabaseAPI();
    ~DatabaseAPI();
    connectToDatabase();
    disconnectFromDatabase();
    getAllElementInfo();

--- New Implementation/ManaCraft/DatabaseAPI.cpp
+++ New Implementation/ManaCraft/DatabaseAPI.cpp
@@ -2,47 +2,74 @@
 
 using namespace ManaCraft::Database;
 
-bool DatabaseAPI::connectToDatabase(std::string db_Name, std::string server, std::string login, std::string password)
-{
-	return false;
+DatabaseAPI::DatabaseAPI() {
+	conn = Connection(false);
 }
 
-void  DatabaseAPI::disconnectFromDatabase()
-{}
+DatabaseAPI::~DatabaseAPI() {
+	disconnectFromDatabase();
+}
+
+bool DatabaseAPI::connectToDatabase(const std::string& db_Name, const std::string& server, const std::string& login, const std::string& password) {
+	return conn.connect(db_Name.c_str(), server.c_str(), login.c_str(), password.c_str());
+}
+
+void DatabaseAPI::disconnectFromDatabase() {
+	if (conn.connected()) {
+		conn.disconnect();
+	}
+}
+
+void  DatabaseAPI::getTowerInfo(const TowerTypes tower) {
+}
+
+void  DatabaseAPI::getAllTowerInfo() { }
 
 
-void  DatabaseAPI::getTowerInfo(TowerTypes tower)
-{}
+void  DatabaseAPI::getMinionInfo(const MinionTypes minion) { }
 
-void  DatabaseAPI::getAllTowerInfo()
-{}
+void  DatabaseAPI::getAllMinionInfo() { }
 
 
-void  DatabaseAPI::getMinionInfo(MinionTypes minion)
-{}
+void  DatabaseAPI::getElementInfo(const ElementTypes element) { }
 
-void  DatabaseAPI::getAllMinionInfo()
-{}
+std::vector<Element>  DatabaseAPI::getAllElementInfo() {
+	// create helper method for this probably, it'll be repeated a lot
+
+	if (conn.connected()) {
+		try {
+			Query query = conn.query("SELECT * FROM Elements");
+			if (UseQueryResult result = query.use()) {
+				Row row;
+				std::vector<Element> elements = std::vector<Element>();	// do something with this vector
+
+				while (row = result.fetch_row()) {
+					Element* e = new Element();
+					e->type = *(row[1].c_str());	// I think *(c_str()) is right?
+					e->weakness = *(row[2].c_str());
+					e->strength = *(row[3].c_str());
+
+					elements.push_back(*e);
+				}
+				return elements;
+			}
+		}
+		catch (Exception e) {
+			throw e;
+		}
+	}
+	else {
+		// throw custom exception?
+	}
+}
+
+void  DatabaseAPI::getResistanceInfo(const ElementTypes element) { }
+
+void  DatabaseAPI::getAllResistanceInfo() { }
 
 
-void  DatabaseAPI::getElementInfo(ElementTypes element)
-{}
+void  DatabaseAPI::saveGame() { }
 
-void  DatabaseAPI::getAllElementInfo()
-{}
+void  DatabaseAPI::getSaveGame(const unsigned int& saveID) { }
 
-void  DatabaseAPI::getResistanceInfo(ElementTypes element)
-{}
-
-void  DatabaseAPI::getAllResistanceInfo()
-{}
-
-
-void  DatabaseAPI::saveGame()
-{}
-
-void  DatabaseAPI::getSaveGame(unsigned int saveID)
-{}
-
-void  DatabaseAPI::getPlayerInfo(std::string name)
-{}
+void  DatabaseAPI::getPlayerInfo(const std::string& name) { }
--- New Implementation/ManaCraft/DatabaseAPI.h
+++ New Implementation/ManaCraft/DatabaseAPI.h
@@ -2,15 +2,34 @@
 #define DATABASE_API_H
 
 #include <string>
+#include <vector>	// possibly not needed later
+#include <mysql++.h>
 #include "DataDefinitions.h"
+
+#include "Element.h" // change to DataInclude.h or something
+
+//		NOTES FROM VINNY
+////////////////////////////////////////////////////////////
+//	- If we're not passing a reference to the local DB, we could maybe make this mostly static? And remove constructors.
+//	- Also if so, functions need return types
+//	- Also also, could rename functions for default data to "fetch..." to avoid confusion between default values and saved values. Unless those can use the same functions.
+//	- Consider passing just the ID for Tower/Minion parameters
+//	- Possibly add DBNotConnectedException for functions? or sumin'
+//	- Consider removing connectToDatabase parameters and using private constants
 
 namespace ManaCraft {
 	namespace Database {
 
-		class DatabaseAPI
-		{
+		using namespace mysqlpp;
+		class DatabaseAPI {
+
+		private:
+			Connection conn;
 
 		public:
+
+			DatabaseAPI();
+			~DatabaseAPI();
 
 			/// <summary> Establishes a connection to a database.
 			/// <para>[string] Name of the data base to connect to. </para>
@@ -19,8 +38,8 @@ namespace ManaCraft {
 			/// <para>[string] Login password. </para>
 			/// <returns>[Returns] a bool. True if connection was established. </returns>
 			/// </summary>
-			bool connectToDatabase(std::string db_Name, std::string server, std::string login, std::string password);
-			
+			bool connectToDatabase(const std::string& db_Name, const std::string& server, const std::string& login, const std::string& password);
+
 			/// <summary> Disconnects from the database if a connection is already established. </summary>
 			void disconnectFromDatabase();
 
@@ -28,34 +47,34 @@ namespace ManaCraft {
 			//TODO: Proper returns for methods below
 
 			/// <summary> Get information of specific tower of TowerType. </summary>
-			void getTowerInfo(TowerTypes tower);
+			void getTowerInfo(const TowerTypes tower);
 			/// <summary> Get information on all the Towers. </summary>
 			void getAllTowerInfo();
 
 			/// <summary> Get information of specific minion of MinionType. </summary>
-			void getMinionInfo(MinionTypes minion);
+			void getMinionInfo(const MinionTypes minion);
 			// <summary> Get information on all Minions. </summary>
 			void getAllMinionInfo();
 
 			/// <summary> Get information of specific element of ElementType. </summary>
-			void getElementInfo(ElementTypes element);
+			void getElementInfo(const ElementTypes element);
 			// <summary> Get information on all Elements. </summary>
-			void getAllElementInfo();
+			std::vector<Element> getAllElementInfo();
 
 			/// <summary> Get information of specific Resistance of ElementType. </summary>
-			void getResistanceInfo(ElementTypes element);
+			void getResistanceInfo(const ElementTypes element);
 			// <summary> Get information on all Resistances. </summary>
 			void getAllResistanceInfo();
-			
+
 			/// <summary> Saves the current game being played. </summary>
 			void saveGame(); //TODO: lol figure how this is going to work
 			/// <summary> Pulls information for a Save Game
-			void getSaveGame(unsigned int saveID);
+			void getSaveGame(const unsigned int& saveID);
 
 			/// <summary> Get a players information from the database.
 			/// <para>[string] Username of the player.
 			/// </summary>
-			void getPlayerInfo(std::string name);
+			void getPlayerInfo(const std::string& name);
 
 		};
 	}
