e08acd3d8f4539a0e25c6e1f82fd29aec209a20b
Tyler Remazki <tremazki@hotmail.com>
2014-02-27 19:09:54 -0500

    Basic particle system implementation > Create Particle System object > Call which type of emitter (Currently only burst) > Add to GUIElement list

--- New Implementation/ManaCraft/ParticleSystem.cpp
+++ New Implementation/ManaCraft/ParticleSystem.cpp
@@ -1,24 +1,36 @@
 #include "ParticleSystem.h"
 
-ParticleSystem::ParticleSystem(size_t _numberOfParticles, SDL_Texture* _texture)
+ParticleSystem::ParticleSystem(size_t _numberOfParticles, SDL_Texture* _texture, bool _randomSpeeds)
 {
 	particleTexture = _texture;
 	particleNum = _numberOfParticles;
+	randomSpeeds = _randomSpeeds;
 	Initialize();
 }
-
-ParticleSystem::~ParticleSystem(){
-
+ParticleSystem::ParticleSystem(size_t _numberOfParticles, SDL_Texture* _texture, float _particleSpeed)
+{
+	randomSpeeds = false;
+	particleTexture = _texture;
+	particleNum = _numberOfParticles;
+	particleSpeed = _particleSpeed;
+	Initialize();
 }
+ParticleSystem::~ParticleSystem(){}
 
 void ParticleSystem::Initialize()
 {
-	particleList = std::vector<Particle*>();
 	for (size_t i = 0; i < particleNum; i++)
 	{
 		if (particleTexture != nullptr)
 		{
-			particleList.push_back(new Particle(particleTexture));
+			if (!randomSpeeds)
+			{
+				particleList.push_back(new Particle(particleTexture, particleSpeed));
+			}
+			else
+			{
+				particleList.push_back(new Particle(particleTexture));
+			}
 		}
 	}
 }
@@ -31,20 +43,38 @@ void ParticleSystem::Draw()
 		rect.y += offset.y + padding.y;
 		for (size_t i = 0; i < particleList.size(); i++)
 		{
-			particleList.at(i)->Draw();
+			if (particleList.at(i)->isAlive())
+				particleList.at(i)->Draw();
 		}
 	}
 }
 
-void ParticleSystem::Update(double time){
+void ParticleSystem::Update(double time)
+{
 	if (Active)
 	{
-		for (size_t i = 0; i < particleList.size(); i++)
+		if (particleList.size() != 0)
 		{
-			//TODO
-			//Get the direction each particle has to move in
-			//Add a velocity to this directional vector
-			particleList.at(i)->Update(time);
+			for (size_t i = 0; i < particleList.size(); i++)
+			{
+				if (particleList.at(i)->isAlive())
+					particleList.at(i)->Update(time);
+				else
+				{
+					particleList.erase(particleList.begin() + i);
+				}
+			}
 		}
+	}
+}
+
+void ParticleSystem::Burst()
+{
+	//Spread the particles out along a circle
+	for (size_t i = 0; i < particleList.size(); i++)
+	{
+		float inDegrees = 360 / particleNum;
+		float inRadians = inDegrees * (3.14f / 180);
+		particleList[i]->SetDirection(sin(inRadians*i), cos(inRadians*i));
 	}
 }
--- New Implementation/ManaCraft/ParticleSystem.h
+++ New Implementation/ManaCraft/ParticleSystem.h
@@ -3,55 +3,103 @@
 #define PARTICLE_SYSTEM_H
 
 #include "GuiElement.h"
-#include <vector>
+#include <math.h>
+
+//Vector Struct
+struct Vector2{
+	float x, y;
+	Vector2(float _x, float _y) : x(_x), y(_y) {};
+	Vector2();
+};
+
+//Particle class - For use only within Particle System
 class Particle : public GuiElement{
 public:
-	Particle(){
-		texture = nullptr;
-	}
-
-	Particle(SDL_Texture* _texture){
+	Particle(){}
+	Particle(SDL_Texture* _texture, float _speed)
+	{
+		alive = true;
+		alpha = 255;
 		texture = _texture;
+		speed = _speed;
 		SDL_QueryTexture(texture, NULL, NULL, &rectWidth, &rectHeight); //Grab the texture data and set the width and height of our dst rectangle to the textures dimensions
-		rect = {100 , 100, rectWidth, rectHeight };
+		rect = { 500, 500, rectWidth, rectHeight };
+		SetDirection(0, 0);
 	}
-	virtual void Free(){}
+	Particle(SDL_Texture* _texture)
+	{
+		alive = true;
+		alpha = 255;
+		texture = _texture;
+		speed = rand() % 20;
+		SDL_QueryTexture(texture, NULL, NULL, &rectWidth, &rectHeight); //Grab the texture data and set the width and height of our dst rectangle to the textures dimensions
+		rect = { 500, 500, rectWidth, rectHeight };
+		SetDirection(0, 0);
+	}
 	virtual void Update(double time)
 	{
 		if (Active)
 		{
 			//Offsets and padding prior to updating 
-			rect.x += offset.x + padding.x;
-			rect.y += offset.y + padding.y;
+			if (alive)
+			{
 
-			rect.y--;
+				rect.x += offset.x + padding.x;
+				rect.y += offset.y + padding.y;
+				SetPosition((int)GetPosition().x + (directionX * speed), //Set the position of the particle and update based on its directional vector and speed
+					(int)GetPosition().y + (directionY * speed));
+				
+				//Reduce the alpha over time
+				if (alpha >= 10){
+					alpha -= time / 20;
+				}else{
+					alpha = 0;
+				}
+				if (alpha == 0){ alive = false; } //If the alpha is equal to zero, the particle is no longer living
+
+				SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND); //Change blending mode of the texture
+				SDL_SetTextureAlphaMod(texture, alpha); //Set the alpha of the texture
+			}
+
 		}
 	}
-
 	virtual void Draw()
 	{
 		if (Active) 
 		{
-			rect.x += offset.x + padding.x;
-			rect.y += offset.y + padding.y;
-			SDL_RenderCopyEx(Window::Renderer(), texture, NULL, &rect, 0, NULL, flip);
+			if (alive)
+			{
+				rect.x += offset.x + padding.x;
+				rect.y += offset.y + padding.y;
+				SDL_RenderCopyEx(Window::Renderer(), texture, NULL, &rect, 0, NULL, flip);
+			}
 		}
 	}
+
+	float GetDirectionX() { return directionX; }
+	float GetDirectionY() { return directionY; }
+	void SetDirection(float x, float y){ directionX = x; directionY = y; }
+
+	bool isAlive() { return alive; }
 private:
 	int rectWidth, rectHeight;
-
+	float directionX, directionY, speed, alpha;
+	bool alive;
 };
 
 class ParticleSystem : public GuiElement{
 public:
 	ParticleSystem() : particleNum(0) {};
-	ParticleSystem(size_t numberOfParticles, SDL_Texture* texture);
+	ParticleSystem(size_t numberOfParticles, SDL_Texture* texture, bool randomSpeeds);
+	ParticleSystem(size_t numberOfParticles, SDL_Texture* texture, float particleSpeed);
 	~ParticleSystem();
+	void Burst();
 private:
 	size_t particleNum;
+	float particleSpeed;
+	bool randomSpeeds;
 	SDL_Texture* particleTexture;
 	std::vector<Particle*> particleList;
-
 	virtual void Update(double time);
 	virtual void Draw();
 	void Initialize();
