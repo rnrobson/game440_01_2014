f8c28774599871c332388bda78db11b85135451e
Tyler Remazki <tremazki@hotmail.com>
2014-03-05 15:05:22 -0500

    Updating ParticleSystem --Incomplete--

--- New Implementation/ManaCraft/ParticleSystem.cpp
+++ New Implementation/ManaCraft/ParticleSystem.cpp
@@ -1,23 +1,22 @@
 #include "ParticleSystem.h"
 
+//ParticleSystem::DIRECTION;
 ParticleSystem::ParticleSystem(size_t _numberOfParticles, SDL_Texture* _texture, bool _randomSpeeds)
 {
 	particleTexture = _texture;
-	particleNum = _numberOfParticles;
 	randomSpeeds = _randomSpeeds;
-	Initialize();
+	Initialize(_numberOfParticles);
 }
 ParticleSystem::ParticleSystem(size_t _numberOfParticles, SDL_Texture* _texture, float _particleSpeed)
 {
 	randomSpeeds = false;
 	particleTexture = _texture;
-	particleNum = _numberOfParticles;
 	particleSpeed = _particleSpeed;
-	Initialize();
+	Initialize(_numberOfParticles);
 }
 ParticleSystem::~ParticleSystem(){}
 
-void ParticleSystem::Initialize()
+void ParticleSystem::Initialize(size_t particleNum)
 {
 	for (size_t i = 0; i < particleNum; i++)
 	{
@@ -51,6 +50,7 @@ void ParticleSystem::Draw()
 
 void ParticleSystem::Update(double time)
 {
+	bool particleListOccupied = false;
 	if (Active)
 	{
 		if (particleList.size() != 0)
@@ -58,13 +58,18 @@ void ParticleSystem::Update(double time)
 			for (size_t i = 0; i < particleList.size(); i++)
 			{
 				if (particleList.at(i)->isAlive())
-					particleList.at(i)->Update(time);
-				else
 				{
-					particleList.erase(particleList.begin() + i);
+					particleListOccupied = true;
+					particleList.at(i)->Update(time);
 				}
+
 			}
 		}
+	}
+	if (!particleListOccupied)
+	{
+		particleList.clear();
+		Active = false;
 	}
 }
 
@@ -73,8 +78,34 @@ void ParticleSystem::Burst()
 	//Spread the particles out along a circle
 	for (size_t i = 0; i < particleList.size(); i++)
 	{
-		float inDegrees = 360 / particleNum;
+		float inDegrees = 360 / particleList.size();
 		float inRadians = inDegrees * (3.14f / 180);
 		particleList[i]->SetDirection(sin(inRadians*i), cos(inRadians*i));
 	}
 }
+
+/*void ParticleSystem::Emit(DIRECTION emitterDirection)
+{
+	//Determines which direction the emitter will emit.
+	Vector2 tempEmitterDirection;
+	/*switch (emitterDirection){
+	case UP:
+		tempEmitterDirection = Vector2(0, 1);
+		break;
+	case DOWN:
+		tempEmitterDirection = Vector2(0, -1);
+		break;
+	case LEFT:
+		tempEmitterDirection = Vector2(1, 0);
+		break;
+	case RIGHT:
+		tempEmitterDirection = Vector2(-1, 0);
+		break;
+	}
+
+	for (size_t i = 0; i < particleList.size(); i++)
+	{
+		//particleList.at(i)->SetDirection(tempEmitterDirection.x, 
+			//						     tempEmitterDirection.y);
+	}
+} */
