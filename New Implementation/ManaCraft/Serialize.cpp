f3fc5068168ab562214d48e28658fb56f9db27fa
Philip Diehl <pdd845@hotmail.com>
2014-03-16 20:52:34 -0400

    Updated Serialize to work with vectors<char>.

--- New Implementation/ManaCraft/Serialize.cpp
+++ New Implementation/ManaCraft/Serialize.cpp
@@ -8,10 +8,28 @@ Byte* Serialize::Int8(Byte* data, __int8 num) {
 	return data += sizeof(__int8);
 }
 
+void Serialize::Int8(std::vector<char>& buffer, const unsigned int index, const __int8 num) {
+	assert(buffer.size() > index);
+	__int8* pInt8 = new __int8();
+	*pInt8 = num;
+	char* data = (char*)pInt8;
+	buffer[index] = data[0];
+	delete pInt8;
+}
+
 Byte* Serialize::UInt8(Byte* data, unsigned __int8 num) {
 	unsigned __int8* pUInt8 = (unsigned __int8*)data;
 	*pUInt8 = num;
 	return data += sizeof(unsigned __int8);
+}
+
+void Serialize::UInt8(std::vector<char>& buffer, const unsigned int index, const unsigned __int8 num) {
+	assert(buffer.size() > index);
+	unsigned __int8* pUInt8 = new unsigned __int8();
+	*pUInt8 = num;
+	char* data = (char*)pUInt8;
+	buffer[index] = data[0];
+	delete pUInt8;
 }
 
 Byte* Serialize::Int16(Byte* data, __int16 num) {
@@ -20,10 +38,30 @@ Byte* Serialize::Int16(Byte* data, __int16 num) {
 	return data += sizeof(__int16);
 }
 
+void Serialize::Int16(std::vector<char>& buffer, const unsigned int index, const __int16 num) {
+	assert(buffer.size() > (index + 1));
+	__int16* pInt16 = new __int16();
+	*pInt16 = num;
+	char* data = (char*)pInt16;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	delete pInt16;
+}
+
 Byte* Serialize::UInt16(Byte* data, unsigned __int16 num) {
 	unsigned __int16* pUInt16 = (unsigned __int16*)data;
 	*pUInt16 = num;
 	return data += sizeof(unsigned __int16);
+}
+
+void Serialize::UInt16(std::vector<char>& buffer, const unsigned int index, const unsigned __int16 num) {
+	assert(buffer.size() > (index + 1));
+	unsigned __int16* pUInt16 = new unsigned __int16();
+	*pUInt16 = num;
+	char* data = (char*)pUInt16;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	delete pUInt16;
 }
 
 Byte* Serialize::Int32(Byte* data, __int32 num) {
@@ -32,10 +70,34 @@ Byte* Serialize::Int32(Byte* data, __int32 num) {
 	return data += sizeof(__int32);
 }
 
+void Serialize::Int32(std::vector<char>& buffer, const unsigned int index, const __int32 num) {
+	assert(buffer.size() > (index + 3));
+	__int32* pInt32 = new __int32();
+	*pInt32 = num;
+	char* data = (char*)pInt32;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+	delete pInt32;
+}
+
 Byte* Serialize::UInt32(Byte* data, unsigned __int32 num) {
 	unsigned __int32* pUInt32 = (unsigned __int32*)data;
 	*pUInt32 = num;
 	return data += sizeof(unsigned __int32);
+}
+
+void Serialize::UInt32(std::vector<char>& buffer, const unsigned int index, const unsigned __int32 num) {
+	assert(buffer.size() > (index + 3));
+	unsigned __int32* pUInt32 = new unsigned __int32();
+	*pUInt32 = num;
+	char* data = (char*)pUInt32;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+	delete pUInt32;
 }
 
 Byte* Serialize::Int64(Byte* data, __int64 num) {
@@ -44,10 +106,44 @@ Byte* Serialize::Int64(Byte* data, __int64 num) {
 	return data += sizeof(__int64);
 }
 
+void Serialize::Int64(std::vector<char>& buffer, const unsigned int index, const __int64 num) {
+	assert(buffer.size() > (index + 7));
+	__int64* pInt64 = new __int64();
+	*pInt64 = num;
+	char* data = (char*)pInt64;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+	
+	buffer[index + 4] = data[4];
+	buffer[index + 5] = data[5];
+	buffer[index + 6] = data[6];
+	buffer[index + 7] = data[7];
+	delete pInt64;
+}
+
 Byte* Serialize::UInt64(Byte* data, unsigned __int64 num) {
 	unsigned __int64* pUInt64 = (unsigned __int64*)data;
 	*pUInt64 = num;
 	return data += sizeof(unsigned __int64);
+}
+
+void Serialize::UInt64(std::vector<char>& buffer, const unsigned int index, const unsigned __int64 num) {
+	assert(buffer.size() > (index + 7));
+	unsigned __int64* pUInt64 = new unsigned __int64();
+	*pUInt64 = num;
+	char* data = (char*)pUInt64;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+
+	buffer[index + 4] = data[4];
+	buffer[index + 5] = data[5];
+	buffer[index + 6] = data[6];
+	buffer[index + 7] = data[7];
+	delete pUInt64;
 }
 
 Byte* Serialize::Float(Byte* data, float num) {
@@ -55,9 +151,38 @@ Byte* Serialize::Float(Byte* data, float num) {
 	return data += sizeof(float);
 }
 
+void Serialize::Float(std::vector<char>& buffer, const unsigned int index, const float num) {
+	assert(buffer.size() > (index + 3));
+	float* pFloat = new float();
+	*pFloat = num;
+	char* data = (char*)pFloat;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+	delete pFloat;
+}
+
 Byte* Serialize::Double(Byte* data, double num) {
 	Serialize::Int64(data, *(__int64*)&num);
 	return data += sizeof(double);
+}
+
+void Serialize::Double(std::vector<char>& buffer, const unsigned int index, const double num) {
+	assert(buffer.size() > (index + 7));
+	double* pDouble = new double();
+	*pDouble = num;
+	char* data = (char*)pDouble;
+	buffer[index] = data[0];
+	buffer[index + 1] = data[1];
+	buffer[index + 2] = data[2];
+	buffer[index + 3] = data[3];
+
+	buffer[index + 4] = data[4];
+	buffer[index + 5] = data[5];
+	buffer[index + 6] = data[6];
+	buffer[index + 7] = data[7];
+	delete pDouble;
 }
 
 __int8 Deserialize::Int8(Byte* data) {
@@ -65,9 +190,17 @@ __int8 Deserialize::Int8(Byte* data) {
 	return tempInt8;
 }
 
+__int8 Deserialize::Int8(std::vector<char>& buffer, const unsigned int index) {
+	return *(__int8*)&buffer[index];
+}
+
 unsigned __int8 Deserialize::UInt8(Byte* data) {
 	unsigned __int8 tempUInt8 = *(unsigned __int8*)data;
 	return tempUInt8;
+}
+
+unsigned __int8 Deserialize::UInt8(std::vector<char>& buffer, const unsigned int index) {
+	return *(unsigned __int8*)&buffer[index];
 }
 
 __int16 Deserialize::Int16(Byte* data) {
@@ -75,9 +208,17 @@ __int16 Deserialize::Int16(Byte* data) {
 	return tempInt16;
 }
 
+__int16 Deserialize::Int16(std::vector<char>& buffer, const unsigned int index) {
+	return *(__int16*)&buffer[index];
+}
+
 unsigned __int16 Deserialize::UInt16(Byte* data) {
 	unsigned __int16 tempUInt16 = *(unsigned __int16*)data;
 	return tempUInt16;
+}
+
+unsigned __int16 Deserialize::UInt16(std::vector<char>& buffer, const unsigned int index) {
+	return *(unsigned __int16*)&buffer[index];
 }
 
 __int32 Deserialize::Int32(Byte* data) {
@@ -85,9 +226,17 @@ __int32 Deserialize::Int32(Byte* data) {
 	return tempInt32;
 }
 
+__int32 Deserialize::Int32(std::vector<char>& buffer, const unsigned int index) {
+	return *(__int32*)&buffer[index];
+}
+
 unsigned __int32 Deserialize::UInt32(Byte* data) {
 	unsigned __int32 tempUInt32 = *(unsigned __int32*)data;
 	return tempUInt32;
+}
+
+unsigned __int32 Deserialize::UInt32(std::vector<char>& buffer, const unsigned int index) {
+	return *(unsigned __int32*)&buffer[index];
 }
 
 __int64 Deserialize::Int64(Byte* data) {
@@ -95,9 +244,17 @@ __int64 Deserialize::Int64(Byte* data) {
 	return tempInt64;
 }
 
+__int64 Deserialize::Int64(std::vector<char>& buffer, const unsigned int index) {
+	return *(__int64*)&buffer[index];
+}
+
 unsigned __int64 Deserialize::UInt64(Byte* data) {
 	unsigned __int64 tempUInt64 = *(unsigned __int64*)data;
 	return tempUInt64;
+}
+
+unsigned __int64 Deserialize::UInt64(std::vector<char>& buffer, const unsigned int index) {
+	return *(unsigned __int64*)&buffer[index];
 }
 
 float Deserialize::Float(Byte* data) {
@@ -105,7 +262,15 @@ float Deserialize::Float(Byte* data) {
 	return *(float*)&tempInt32;
 }
 
+float Deserialize::Float(std::vector<char>& buffer, const unsigned int index) {
+	return *(float*)&buffer[index];
+}
+
 double Deserialize::Double(Byte* data) {
 	__int64 tempInt64 = Deserialize::Int64(data);
 	return *(double*)&tempInt64;
+}
+
+double Deserialize::Double(std::vector<char>& buffer, const unsigned int index) {
+	return *(double*)&buffer[index];
 }
--- New Implementation/ManaCraft/Serialize.h
+++ New Implementation/ManaCraft/Serialize.h
@@ -3,6 +3,9 @@
 
 typedef char Byte;
 
+#include <vector>
+#include <cassert>
+
 namespace ManaCraft {
 	namespace Networking {
 		class Serialize {
@@ -14,12 +17,16 @@ namespace ManaCraft {
 			/// </summary>
 			static Byte* Int8(Byte* data, __int8 num);
 
+			static void Int8(std::vector<char>& buffer, const unsigned int index, const __int8 num);
+
 			/// <summary> Converts a 8 bit unsigned integer into 1 Byte.
 			/// <para>[Byte* data] Data should point to 1 pre-allocated Byte.</para>
 			/// <para>[unsigned __int8 num] Num represents a 8 bit unsigned integer.</para>
 			/// <returns>[Returns] a pointer to the data that's been incremented by the sizeof(unsigned __int8) for convenience.</returns>
 			/// </summary>
 			static Byte* UInt8(Byte* data, unsigned __int8 num);
+
+			static void UInt8(std::vector<char>& buffer, const unsigned int index, const unsigned __int8 num);
 
 			/// <summary> Converts a 16 bit integer into 2 chars.
 			/// <para>[Byte* data] Data should point to 2 pre-allocated Bytes.</para>
@@ -28,12 +35,16 @@ namespace ManaCraft {
 			/// </summary>
 			static Byte* Int16(Byte* data, __int16 num);
 
+			static void Int16(std::vector<char>& buffer, const unsigned int index, const __int16 num);
+
 			/// <summary> Converts a 16 bit unsigned integer into 2 chars.
 			/// <para>[Byte* data] Data should point to 2 pre-allocated Bytes.</para>
 			/// <para>[unsigned __int16 num] Num represents a 16 bit unsigned integer.</para>
 			/// <returns>[Returns] a pointer to the data that's been incremented by the sizeof(unsigned __int16) for convenience.</returns>
 			/// </summary>
 			static Byte* UInt16(Byte* data, unsigned __int16 num);
+
+			static void UInt16(std::vector<char>& buffer, const unsigned int index, const unsigned __int16 num);
 
 			/// <summary> Converts a 32 bit integer into 4 chars.
 			/// <para>[Byte* data] Data should point to 4 pre-allocated Bytes.</para>
@@ -42,12 +53,16 @@ namespace ManaCraft {
 			/// </summary>
 			static Byte* Int32(Byte* data, __int32 num);
 
+			static void Int32(std::vector<char>& buffer, const unsigned int index, const __int32 num);
+
 			/// <summary> Converts a 32 bit unsigned integer into 4 chars.
 			/// <para>[Byte* data] Data should point to 4 pre-allocated Bytes.</para>
 			/// <para>[unsigned __int32 num] Num represents a 32 bit unsigned integer.</para>
 			/// <returns>[Returns] a pointer to the data that's been incremented by the sizeof(unsigned __int32) for convenience.</returns>
 			/// </summary>
 			static Byte* UInt32(Byte* data, unsigned __int32 num);
+
+			static void UInt32(std::vector<char>& buffer, const unsigned int index, const unsigned __int32 num);
 
 			/// <summary> Converts a 64 bit integer into 8 chars.
 			/// <para>[Byte* data] Data should point to 8 pre-allocated Bytes.</para>
@@ -56,12 +71,16 @@ namespace ManaCraft {
 			/// </summary>
 			static Byte* Int64(Byte* data, __int64 num);
 
+			static void Int64(std::vector<char>& buffer, const unsigned int index, const __int64 num);
+
 			/// <summary> Converts a 64 bit unsigned integer into 8 chars.
 			/// <para>[Byte* data] Data should point to 8 pre-allocated Bytes.</para>
 			/// <para>[__int64 num] Num represents a 64 bit unsigned integer.</para>
 			/// <returns>[Returns] a pointer to the data that's been incremented by the sizeof(unsigned __int64) for convenience.</returns>
 			/// </summary>
 			static Byte* UInt64(Byte* data, unsigned __int64 num);
+
+			static void UInt64(std::vector<char>& buffer, const unsigned int index, const unsigned __int64 num);
 
 			/// <summary> Converts a 32 bit float into 4 chars.
 			/// <para>[Byte* data] Data should point to 4 pre-allocated Bytes.</para>
@@ -70,12 +89,16 @@ namespace ManaCraft {
 			/// </summary>
 			static Byte* Float(Byte* data, float num);
 
+			static void Float(std::vector<char>& buffer, const unsigned int index, const float num);
+
 			/// <summary>Converts a 64 bit double into 8 chars.
 			/// <para>[Byte* data] Data should point to 8 pre-allocated Bytes.</para>
 			/// <para>[double num] Num represents a 64 bit floating point number.</para>
 			/// <returns>[Returns] a pointer to the data that's been incremented by the sizeof(double) for convenience.</returns>
 			/// </summary>
 			static Byte* Double(Byte* data, double num);
+
+			static void Double(std::vector<char>& buffer, const unsigned int index, const double num);
 
 		private:
 			Serialize();
@@ -90,11 +113,15 @@ namespace ManaCraft {
 			/// </summary>
 			static __int8 Int8(Byte* data);
 
+			static __int8 Int8(std::vector<char>& buffer, const unsigned int index);
+
 			/// <summary> Converts 1 Byte into a 8 bit unsigned integer.
 			/// <para>[Byte* data] Data should point to a 8 bit unsigned integer that's currently stored as 1 char.</para>
 			/// <returns>[Returns] the deserialized 8 bit unsigned integer.</returns>
 			/// </summary>
 			static unsigned __int8 UInt8(Byte* data);
+
+			static unsigned __int8 UInt8(std::vector<char>& buffer, const unsigned int index);
 
 			/// <summary> Converts 2 Bytes into a 16 bit integer.
 			/// <para>[Byte* data] Data should point to a 16 bit integer that's currently stored as 2 chars.</para>
@@ -102,11 +129,15 @@ namespace ManaCraft {
 			/// </summary>
 			static __int16 Int16(Byte* data);
 
+			static __int16 Int16(std::vector<char>& buffer, const unsigned int index);
+
 			/// <summary> Converts 2 Bytes into a 16 bit unsigned integer.
 			/// <para>[Byte* data] Data should point to a 16 bit unsigned integer that's currently stored as 2 chars.</para>
 			/// <returns>[Returns] the deserialized 16 bit unsigned integer.</returns>
 			/// </summary>
 			static unsigned __int16 UInt16(Byte* data);
+
+			static unsigned __int16 UInt16(std::vector<char>& buffer, const unsigned int index);
 
 			/// <summary> Converts 4 Bytes into a 32 bit integer.
 			/// <para>[Byte* data] Data should point to a 32 bit integer that's currently stored as 4 chars.</para>
@@ -114,11 +145,15 @@ namespace ManaCraft {
 			/// </summary>
 			static __int32 Int32(Byte* data);
 
+			static __int32 Int32(std::vector<char>& buffer, const unsigned int index);
+
 			/// <summary> Converts 4 Bytes into a 32 bit unsigned integer.
 			/// <para>[Byte* data] Data should point to a 32 bit unsigned integer that's currently stored as 4 chars.</para>
 			/// <returns>[Returns] the deserialized 32 bit unsigned integer.</returns>
 			/// </summary>
 			static unsigned __int32 UInt32(Byte* data);
+
+			static unsigned __int32 UInt32(std::vector<char>& buffer, const unsigned int index);
 
 			/// <summary> Converts 8 Bytes into a 64 bit integer.
 			/// <para>[Byte* data] Data should point to a 64 bit integer that's currently stored as 8 chars.</para>
@@ -126,11 +161,15 @@ namespace ManaCraft {
 			/// </summary>
 			static __int64 Int64(Byte* data);
 
+			static __int64 Int64(std::vector<char>& buffer, const unsigned int index);
+
 			/// <summary> Converts 8 Bytes into a 64 bit unsigned integer.
 			/// <para>[Byte* data] Data should point to a 64 bit unsigned integer that's currently stored as 8 chars.</para>
 			/// <returns>[Returns] the deserialized 64 bit unsigned integer.</returns>
 			/// </summary>
 			static unsigned __int64 UInt64(Byte* data);
+
+			static unsigned __int64 UInt64(std::vector<char>& buffer, const unsigned int index);
 
 			/// <summary> Converts 4 Bytes into a 32 bit float.
 			/// <para>[Byte* data] Data should point to a 32 bit float that's currently stored as 4 chars.</para>
@@ -138,11 +177,15 @@ namespace ManaCraft {
 			/// </summary>
 			static float Float(Byte* data);
 
+			static float Float(std::vector<char>& buffer, const unsigned int index);
+
 			/// <summary> Converts 8 Bytes into a 64 bit double.
 			/// <para>[Byte* data] Data should point to a 64 bit double that's currently stored as 8 chars.</para>
 			/// <returns>[Returns] the deserialized 64 bit double. </returns>
 			/// </summary>
 			static double Double(Byte* data);
+
+			static double Double(std::vector<char>& buffer, const unsigned int index);
 
 		private:
 			Deserialize();
