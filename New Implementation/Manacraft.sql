00aa182c7eeaf30cd91c06274f2f5ab7c16152a4
Robert Evola <robert.evola@gmail.com>
2014-03-20 21:38:27 -0400

    Save/Load Game UML, Updated SQL script, Updated Scrum

--- New Implementation/Manacraft.sql
+++ New Implementation/Manacraft.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 28, 2014 at 02:27 AM
+-- Generation Time: Mar 21, 2014 at 01:33 AM
 -- Server version: 5.5.33
 -- PHP Version: 5.4.4-14+deb7u7
 
@@ -48,6 +48,31 @@ INSERT INTO `Elements` (`ID`, `Type`, `Weakness`, `Strength`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Game`
+--
+
+CREATE TABLE IF NOT EXISTS `Game` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Game_Teams`
+--
+
+CREATE TABLE IF NOT EXISTS `Game_Teams` (
+  `GameID` int(10) unsigned NOT NULL,
+  `TeamID` int(10) unsigned NOT NULL,
+  KEY `GameID` (`GameID`,`TeamID`),
+  KEY `TeamID` (`TeamID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Minions`
 --
 
@@ -60,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `Minions` (
   `Damage` int(10) unsigned NOT NULL,
   `Armour` int(10) unsigned NOT NULL,
   `Speed` int(10) unsigned NOT NULL,
-  `ManaPerSecond` int(10) unsigned NOT NULL,
+  `ManaPerSecond` float unsigned NOT NULL,
   `Cost` int(10) unsigned NOT NULL,
   `SummonCost` int(10) unsigned NOT NULL,
   `UpgradeCost` int(10) unsigned NOT NULL,
@@ -74,21 +99,34 @@ CREATE TABLE IF NOT EXISTS `Minions` (
 --
 
 INSERT INTO `Minions` (`ID`, `Element`, `Name`, `Level`, `Health`, `Damage`, `Armour`, `Speed`, `ManaPerSecond`, `Cost`, `SummonCost`, `UpgradeCost`, `ResearchCost`) VALUES
-(1, 1, 'Blazelings', 1, 4, 1, 2, 20, 0, 30, 0, 0, 0),
-(2, 1, 'Blaze Elemental', 2, 16, 2, 4, 25, 1, 60, 0, 0, 0),
-(3, 1, 'Fire Lords', 3, 33, 4, 8, 30, 33, 120, 0, 0, 0),
-(4, 2, 'Sea Spray', 1, 10, 3, 2, 20, 0, 30, 0, 0, 0),
-(5, 2, 'Water Elemental', 2, 40, 6, 3, 25, 1, 60, 0, 0, 0),
-(6, 2, 'Tidal Lord', 3, 85, 12, 7, 30, 1, 120, 0, 0, 0),
-(7, 3, 'Pebble', 1, 15, 3, 3, 15, 0, 30, 0, 0, 0),
-(8, 3, 'Rock Elemental', 2, 55, 6, 5, 20, 1, 60, 0, 0, 0),
-(9, 3, 'Granite Lord', 3, 120, 12, 9, 25, 1, 120, 0, 0, 0),
-(10, 4, 'Gust', 1, 12, 3, 2, 25, 0, 30, 0, 0, 0),
-(11, 4, 'Wind Elemental', 2, 48, 6, 4, 32, 1, 60, 0, 0, 0),
-(12, 4, 'Storm Lord', 3, 100, 12, 8, 40, 1, 120, 0, 0, 0),
-(13, 5, 'Garden Spider', 1, 10, 2, 1, 20, 0, 20, 0, 0, 0),
-(14, 5, 'Tarantula', 2, 30, 4, 3, 20, 0, 40, 0, 0, 0),
+(1, 1, 'Blazelings', 1, 4, 1, 2, 20, 0.2, 30, 0, 0, 0),
+(2, 1, 'Blaze Elemental', 2, 16, 2, 4, 25, 0.6, 60, 0, 0, 0),
+(3, 1, 'Fire Lords', 3, 33, 4, 8, 30, 1.2, 120, 0, 0, 0),
+(4, 2, 'Sea Spray', 1, 10, 3, 2, 20, 0.3, 30, 0, 0, 0),
+(5, 2, 'Water Elemental', 2, 40, 6, 3, 25, 0.6, 60, 0, 0, 0),
+(6, 2, 'Tidal Lord', 3, 85, 12, 7, 30, 1.2, 120, 0, 0, 0),
+(7, 3, 'Pebble', 1, 15, 3, 3, 15, 0.3, 30, 0, 0, 0),
+(8, 3, 'Rock Elemental', 2, 55, 6, 5, 20, 0.6, 60, 0, 0, 0),
+(9, 3, 'Granite Lord', 3, 120, 12, 9, 25, 1.2, 120, 0, 0, 0),
+(10, 4, 'Gust', 1, 12, 3, 2, 25, 0.3, 30, 0, 0, 0),
+(11, 4, 'Wind Elemental', 2, 48, 6, 4, 32, 0.6, 60, 0, 0, 0),
+(12, 4, 'Storm Lord', 3, 100, 12, 8, 40, 1.2, 120, 0, 0, 0),
+(13, 5, 'Garden Spider', 1, 10, 2, 1, 20, 0.2, 20, 0, 0, 0),
+(14, 5, 'Tarantula', 2, 30, 4, 3, 20, 0.4, 40, 0, 0, 0),
 (15, 5, 'Goliath Spider', 3, 90, 7, 7, 15, 1, 100, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Player`
+--
+
+CREATE TABLE IF NOT EXISTS `Player` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Name` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -121,6 +159,49 @@ INSERT INTO `Resistances` (`ID`, `Type`, `WindResistance`, `FireResistance`, `Wa
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Team`
+--
+
+CREATE TABLE IF NOT EXISTS `Team` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Team_Players`
+--
+
+CREATE TABLE IF NOT EXISTS `Team_Players` (
+  `TeamID` int(10) unsigned NOT NULL,
+  `PlayerID` int(10) unsigned NOT NULL,
+  UNIQUE KEY `PlayerID` (`PlayerID`),
+  KEY `TeamID` (`TeamID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testTable`
+--
+
+CREATE TABLE IF NOT EXISTS `testTable` (
+  `num` int(11) DEFAULT NULL,
+  `str` varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `testTable`
+--
+
+INSERT INTO `testTable` (`num`, `str`) VALUES
+(1, 'one');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Towers`
 --
 
@@ -130,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `Towers` (
   `Name` varchar(20) NOT NULL,
   `Damage` int(10) unsigned NOT NULL,
   `Range` int(10) unsigned NOT NULL,
-  `FiringRate` int(10) unsigned NOT NULL,
+  `FiringRate` float unsigned NOT NULL,
   `AttackType` varchar(20) NOT NULL,
   `Cost` int(10) unsigned NOT NULL,
   `SummonCost` int(10) unsigned NOT NULL,
@@ -146,25 +227,32 @@ CREATE TABLE IF NOT EXISTS `Towers` (
 --
 
 INSERT INTO `Towers` (`ID`, `Element`, `Name`, `Damage`, `Range`, `FiringRate`, `AttackType`, `Cost`, `SummonCost`, `UpgradeCost`, `ResearchCost`, `SellPrice`) VALUES
-(1, 1, 'Flame', 3, 5, 1, 'SingleTarget', 50, 0, 0, 0, 0),
-(2, 1, 'Fireball', 7, 3, 0, 'GroundTarget-AOE', 120, 0, 0, 0, 0),
-(3, 1, 'Igniter', 5, 5, 1, 'SingleTarget', 100, 0, 0, 0, 0),
-(4, 2, 'Water Blast', 3, 5, 1, 'SingleTarget', 50, 0, 0, 0, 0),
-(5, 2, 'Tidal Wave', 2, 3, 1, 'Cone-AOE', 120, 0, 0, 0, 0),
-(6, 2, 'Frost Beam', 6, 4, 2, 'SingleTarget', 100, 0, 0, 0, 0),
-(7, 3, 'Rock Shard', 4, 4, 1, 'SingleTarget', 50, 0, 0, 0, 0),
-(8, 3, 'Earthquake', 6, 2, 1, 'PointBlank-AOE', 120, 0, 0, 0, 0),
-(9, 3, 'Boulder', 9, 4, 1, 'SingleTarget', 100, 0, 0, 0, 0),
-(10, 4, '50', 2, 4, 2, 'SingleTarget', 50, 0, 0, 0, 0),
-(11, 4, 'Lightning', 3, 5, 1, 'Chain-AOE', 120, 0, 0, 0, 0),
+(1, 1, 'Flame', 3, 5, 1.2, 'SingleTarget', 50, 0, 0, 0, 0),
+(2, 1, 'Fireball', 7, 3, 0.3, 'GroundTarget-AOE', 120, 0, 0, 0, 0),
+(3, 1, 'Igniter', 5, 5, 1.3, 'SingleTarget', 100, 0, 0, 0, 0),
+(4, 2, 'Water Blast', 3, 5, 1.3, 'SingleTarget', 50, 0, 0, 0, 0),
+(5, 2, 'Tidal Wave', 2, 3, 0.5, 'Cone-AOE', 120, 0, 0, 0, 0),
+(6, 2, 'Frost Beam', 6, 4, 1.5, 'SingleTarget', 100, 0, 0, 0, 0),
+(7, 3, 'Rock Shard', 4, 4, 1.1, 'SingleTarget', 50, 0, 0, 0, 0),
+(8, 3, 'Earthquake', 6, 2, 0.5, 'PointBlank-AOE', 120, 0, 0, 0, 0),
+(9, 3, 'Boulder', 9, 4, 1.3, 'SingleTarget', 100, 0, 0, 0, 0),
+(10, 4, 'Gust', 2, 4, 1.9, 'SingleTarget', 50, 0, 0, 0, 0),
+(11, 4, 'Lightning', 3, 5, 1.2, 'Chain-AOE', 120, 0, 0, 0, 0),
 (12, 4, 'Wind Slash', 6, 4, 2, 'SingleTarget', 100, 0, 0, 0, 0),
 (13, 5, 'Arrow', 2, 4, 2, 'SingleTarget', 40, 0, 0, 0, 0),
-(14, 5, 'Cannon', 4, 3, 1, 'AOE', 90, 0, 0, 0, 0),
+(14, 5, 'Cannon', 4, 3, 0.5, 'AOE', 90, 0, 0, 0, 0),
 (15, 5, 'Ballista', 5, 5, 2, 'SingleTarget', 80, 0, 0, 0, 0);
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `Game_Teams`
+--
+ALTER TABLE `Game_Teams`
+  ADD CONSTRAINT `Game_Teams_ibfk_2` FOREIGN KEY (`TeamID`) REFERENCES `Team` (`ID`),
+  ADD CONSTRAINT `Game_Teams_ibfk_1` FOREIGN KEY (`GameID`) REFERENCES `Game` (`ID`);
 
 --
 -- Constraints for table `Minions`
@@ -177,6 +265,13 @@ ALTER TABLE `Minions`
 --
 ALTER TABLE `Resistances`
   ADD CONSTRAINT `Resistances_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `Elements` (`ID`);
+
+--
+-- Constraints for table `Team_Players`
+--
+ALTER TABLE `Team_Players`
+  ADD CONSTRAINT `Team_Players_ibfk_2` FOREIGN KEY (`PlayerID`) REFERENCES `Player` (`ID`),
+  ADD CONSTRAINT `Team_Players_ibfk_1` FOREIGN KEY (`TeamID`) REFERENCES `Team` (`ID`);
 
 --
 -- Constraints for table `Towers`
